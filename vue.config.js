efb5cc38c9e57ba2b0b9f56f023b0b1267325d4b
Fournier Camille <cmffournier@gmail.com>
2020-07-22 19:50:00 +0200

    rewind

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,3 @@
-const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
-
-
 module.exports = {
   transpileDependencies: ["vuetify"],
-  chainWebpack: (config) => {
-    config.plugin("VuetifyLoaderPlugin").tap((args) => [
-      {
-        match(originalTag, { kebabTag, camelTag, path, component }) {
-          if (kebabTag.startsWith("core-")) {
-            return [
-              camelTag,
-              `import ${camelTag} from '@/components/core/${camelTag.substring(
-                4
-              )}.vue'`,
-            ];
-          }
-        },
-      },
-    ]);
-  },
 };
